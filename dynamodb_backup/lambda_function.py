d6e144ddd40c80dd6fe39789370182b390345fab
kachan777 <hoge@gmail.com>
2017-12-25 23:03:33 +0900

    create new class

--- dynamodb_backup/lambda_function.py
+++ dynamodb_backup/lambda_function.py
@@ -1,21 +1,52 @@
 import boto3
 import datetime
+import json
+
+class Backup:
+
+    def __init__(self):
+        # dynamodbに接続
+        self.dynamodb = boto3.resource('dynamodb')
+        self.client = boto3.client('dynamodb')
+
+    def table_scan(self, table_name):
+        table = self.dynamodb.Table(table_name)
+        try:
+            res = table.scan()
+            return res
+        except Exception as e:
+            return e
+
+    def backup_exe(self, name, today):
+        print(name)
+        res = self.client.create_backup(
+            TableName = name,
+            BackupName = name + '.' + today
+        )
+        return res
+
+    def check(self):
+        pass
+
+    def lotate(self):
+        pass
 
 def lambda_handler(event, context):
 
-    # dynamodbに接続
-    dynamodb = boto3.client('dynamodb')
-
-    # table listを取得
-    table_list = dynamodb.list_tables()['TableNames']
-
     # バックアップファイル名に付与する日付のsuffix
-
     now = datetime.datetime.now()
     today = now.strftime("%Y%m%d")
 
-    for table in table_list:
-        response = dynamodb.create_backup(
-            TableName = table,
-            BackupName = table + '.' + today
-        )
+    # バックアップ対象のリストが定義されているdynamodb table
+    backup_list = 'backup_status'
+
+    print('Loading function')
+
+    # バックアップインスタンス作成
+    backup = Backup()
+
+    # バックアップリスト読み出し & バックアップ実行
+    for keys in backup.table_scan(backup_list)['Items']:
+        print(keys['name'])
+        backup.backup_exe(keys['name'], today)
+
