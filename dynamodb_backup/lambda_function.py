3dfb9d7235307a28512189d24d04af168d7fae3a
Takeshi Katsumata <katsumata@PC0034.local>
2018-03-02 21:52:30 +0900

    update lambda function

--- dynamodb_backup/lambda_function.py
+++ dynamodb_backup/lambda_function.py
@@ -1,97 +1,110 @@
 import boto3
 import datetime
-import json
+import dateutil.parser
+import os
+from time import sleep
 
 class Backup:
 
     def __init__(self):
         # dynamodbに接続
-        #self.dynamodb = boto3.resource('dynamodb')
-        #self.client = boto3.client('dynamodb')
-        pass
+        self.client = boto3.client('dynamodb')
 
-    def create_target_list(self):
+        # SNSへ接続
+        self.sns = boto3.client('sns')
 
-        # リスト保存先のS3バケット
-        bucket = 'dynamodb-test'
+        # 実行時間の情報を作成(バックアップファイル名に日付を付与、ファイルローテートの際の比較に使用)
+        self.new_backup_day = datetime.datetime.now(dateutil.tz.tzlocal())
+        self.filename_suffix = self.new_backup_day.strftime("%Y%m%d")
 
-        # バックアップ対象のリスト名
-        target_list = 'dynamodb_backup_target.list'
+        # 環境変数より日次バックアップの世代数を読み込み
+        self.backup_generation = int(os.environ['backup_generation'])
 
-        # s3へ接続
-        s3 = boto3.resource('s3')
+        # 環境変数よりバックアップ対象のテーブルを読み込み、リストを作成
+        self.target_list = os.environ['tables'].split(",")
 
-        # s3にあるバックアップリストを/tmpヘコピー
-        s3.Bucket(bucket).download_file(target_list, '/tmp/dynamodb_backup_target.list')
 
-        target_list = []
+    def execute(self):
+        response = ""
+        # リスト"target_list"からバックアップ対象のテーブルを読み込み、バックアップを順次実行
+        for name in self.target_list:
+            try:
+                self.client.create_backup(
+                    TableName = name,
+                    BackupName = name + '.' + self.filename_suffix
+                )
+                # CreateBackupのAPIは最大50回/秒の制限があるため、スリープを入れる
+                sleep(0.02)
+                response += "Backup job start [" + name + "].\n"
+            except:
+                response += "Backup job failed [" + name + "].\n"
+        print(response)
 
-        with open('/tmp/dynamodb_backup_target.list', 'rt') as f:
-            for line in f:
-                target_list.append(line)
-                print(line)
-
-        return target_list
-
-    def backup_exe(self, target_list, today):
-
-        # dynamodbに接続
-        client = boto3.client('dynamodb')
-
-        for name in target_list:
-            name = name.replace('\n','')
-            res = client.create_backup(
-                TableName = name,
-                BackupName = name + '.' + today
-            )
-        return res
-
-    def lotate(self, target_list, today):
-
-        # dynamodbに接続
-        client = boto3.client('dynamodb')
-
-        # バックアップ情報取得メソッドの引数を作成
-        year = str(today[0:4])
-        month = str(today[4:6])
-        day = str(today[6:8])
-        mod_today = year + ',' + month + ',' + day
-        print(mod_today)
-
-        for name in target_list:
-            name = name.replace('\n','')
-            res = client.list_backups(
-                TableName = name,
-                #TimeRangeLowerBound = datetime.datetime(year + ',' + month + ',' + day)
-                TimeRangeLowerBound = datetime.datetime(2017, 12, 26)
-            )
-        print(res)
-'''
-        if res in 'CREATING':
-            print('Backup job is still in progress...')
+        # responseにメールの件名にジョブの成否を入れるための文字列生成
+        if 'failed' in response:
+            return response, '[Backup job failed] '
         else:
-            response = client.delete_backup(
-                BackupArn = res[]
-            )
-'''
+            return response, '[Backup job start] '
+
+    def rotate(self):
+        response2 = ""
+        # リスト"target_list"からバックアップ対象のテーブルを読み込み、各バックアップファイルごとの情報を取得し、ローテーション処理を実施
+        try:
+            for name in self.target_list:
+                res = self.client.list_backups(
+                    TableName = name
+                )
+                # list_backupsのAPIは最大5回/秒の制限があるため、スリープを入れる
+                sleep(0.2)
+
+                # 最新のバックアップが成功していることを確認
+                if 'AVAILABLE' in res['BackupSummaries'][-1]['BackupStatus']:
+                    response2 += "Backup success! [" + name + "]\n"
+                    # 最新のバックアップが成功しているなら、バックアップの保持期間を比較し、削除対象であれば削除実行
+                    for item in res['BackupSummaries']:
+                        # 検査対象のバックアップファイルのタイムスタンプを取得
+                        old_backup_day =  dateutil.parser.parse(str(item['BackupCreationDateTime']))
+                        # 検査対象と最新のバックアップファイルのタイムスタンプを比較、保持期間を超えているか判断し削除実行
+                        if (self.new_backup_day - old_backup_day).days > self.backup_generation - 1:
+                            self.client.delete_backup(
+                                BackupArn = item['BackupArn']
+                            )
+                            response2 += item['BackupName'] + " has been deleted.\n"
+                            # DeleteBackupのAPIは最大10回/秒の制限があるため、スリープを入れる
+                            sleep(0.1)
+                else:
+                    response2 += "Rotate job failed [" + name + "].\n"
+        except:
+            response2 += "Rotate job failed [" + name + "].\n"
+
+        print(response2)
+
+        # responseにメールの件名にジョブの成否を入れるための文字列生成
+        if 'failed' in response2:
+            return response2, '[Rotate job failed] '
+        else:
+            return response2, '[Rotate job success] '
+
+    def notify(self, text, subject):
+        self.sns.publish(
+            TopicArn = os.environ['SnsTopicArn'],
+            Message = text,
+            Subject = subject
+        )
 
 def lambda_handler(event, context):
 
-    # バックアップファイル名に付与する日付のsuffix
-    now = datetime.datetime.now()
-    today = now.strftime("%Y%m%d")
-
-    print('Loading function')
+    print("Loading function")
 
     # バックアップインスタンス作成
     backup = Backup()
 
-    # バックアップのターゲットリスト読み出し
-    target_list = backup.create_target_list()
+    # バックアップ実行(機能を有効にしたいときはアンコメントしてください)
+    #text = backup.execute()
 
-    # バックアップ実行
-    backup.backup_exe(target_list, today)
+    # バックアップ完了のチェック & 古いバックアップの削除(機能を有効にしたいときはアンコメントしてください)
+    #text = backup.rotate()
 
-    # バックアップ完了のチェック & 古いバックアップの削除
-    backup.lotate(target_list, today)
+    # バックアップ、ローテーションの結果をメール通知
+    backup.notify(text[0], text[1] + context.function_name + " notification")
 
